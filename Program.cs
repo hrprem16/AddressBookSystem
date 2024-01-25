1bff0e60b8345c2e60695554dbb0f84b4f4cf172
Prince Bhagat <hrprem16@gmail.com>
2024-01-25 11:25:24 +0530

    [Prince] Add: Create contact in Address Book

--- Program.cs
+++ Program.cs
@@ -3,9 +3,30 @@ namespace Addressbook
 {
     class program
     {
-        static void Main()
+        class Contact
+        {
+            public string FirstName;
+            public string LastName;
+            public string address;
+            public string city;
+            public int ZipCode;
+            public string number;
+            public string email;
+           public Contact (string FirstName, string LastName, string address, string city, int ZipCode, string number, string email)
+            {
+                this.FirstName = FirstName;
+                this.address = LastName;
+                this.address = address;
+                this.city = city;
+                this.ZipCode = ZipCode;
+                this.number = number;
+                this.email = email;
+            }
+        }
+        static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book");
+            Contact ob1 = new Contact("Prince", "Bhagat","Simri Bakhtiyarpur","Saharsa",852127,"8769944633","hrprem16@gmail.com");
         }
     }
 }
