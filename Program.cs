6779efad3dd5660a3441a75167b4729f3d8a5139
Prince Bhagat <hrprem16@gmail.com>
2024-01-25 12:46:14 +0530

    [Prince] Add: Add a new Contact to Address Book

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 namespace Addressbook
 {
-    class program
+    public class program
     {
         class Contact
         {
@@ -12,10 +12,11 @@ namespace Addressbook
             public int ZipCode;
             public string number;
             public string email;
+        
            public Contact (string FirstName, string LastName, string address, string city, int ZipCode, string number, string email)
             {
                 this.FirstName = FirstName;
-                this.address = LastName;
+                this.LastName = LastName;
                 this.address = address;
                 this.city = city;
                 this.ZipCode = ZipCode;
@@ -23,10 +24,41 @@ namespace Addressbook
                 this.email = email;
             }
         }
+        class Address_Book
+        {
+            List<Contact> contacts;//created list of contact class
+            public Address_Book()
+            {
+                contacts = new List<Contact>();
+            }
+            public void addContact(Contact contact)
+            {
+                contacts.Add(contact);
+            }
+            public void display()
+            {
+             
+                foreach (var contact in contacts)
+                {
+                    Console.WriteLine($"Name : {contact.FirstName} {contact.LastName}");
+                    Console.WriteLine($"Address : {contact.address},{contact.city},{contact.ZipCode}");
+                    Console.WriteLine($"Contact : {contact.number}");
+                    Console.WriteLine($"Email : {contact.email}");
+                }
+            }
+
+
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book");
-            Contact ob1 = new Contact("Prince", "Bhagat","Simri Bakhtiyarpur","Saharsa",852127,"8769944633","hrprem16@gmail.com");
+            Contact obj1 = new Contact("Prince", "Bhagat","Simri Bakhtiyarpur","Saharsa",852127,"8769944633","hrprem16@gmail.com");
+
+            Address_Book book1 = new Address_Book();
+            book1.addContact(obj1);
+            book1.display();
+
+
         }
     }
 }
